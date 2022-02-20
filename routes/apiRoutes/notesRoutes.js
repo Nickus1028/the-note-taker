392ba12d5471a2533d07acf2c878c4be936b24f2
nickus1028 <bentivolio@gmail.com>
2022-02-20 14:33:02 -0500

    Delete button working but not refreshing page

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,29 +1,31 @@
 const router = require("express").Router();
-const { createNewNote, updateDb } = require("../../lib/notes");
+const { createNote, updateDatabase } = require("../../lib/notes");
 const { notes } = require("../../db/db");
 
 
 router.get("/notes", (req, res) => {
-    let results = notes
-    res.json(results)
+    let results = notes;
+    res.json(results);
 })
 
 router.post("/notes", (req, res) => {
-    req.body.id = Math.floor(Math.random() * 10000000000)
-    const note = createNewNote(req.body, notes)
+    req.body.id = Math.floor(Math.random() * 10000000000);
+    const note = createNote(req.body, notes);
     res.json(note);
 })
 
 router.delete("/notes/:id", (req, res) => {
-    const idDeleted = req.params.id.toString();
+    const deleteID = req.params.id;
+    console.log("Delete Button HIT at ID:" + deleteID);
 
     for (let i = 0; i < notes.length; i++) {
-        if (notes[i].id === idDeleted) {
-            notes.splice(i, 1)
-        } 
-    } 
-
-    updateDb(notes);
+        
+        if (notes[i].id = deleteID) {
+            notes.splice(i, 1);
+            console.log("Successful deletion");
+            updateDatabase(notes);
+        } ;
+    };   
 });
 
 
