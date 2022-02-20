43fe7c1753ac8d3f5940c88a631018d0e325fae7
nickus1028 <bentivolio@gmail.com>
2022-02-20 15:10:03 -0500

    Fixed deletion to update page

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -3,6 +3,7 @@ const { createNote, updateDatabase } = require("../../lib/notes");
 const { notes } = require("../../db/db");
 
 
+
 router.get("/notes", (req, res) => {
     let results = notes;
     res.json(results);
@@ -19,12 +20,13 @@ router.delete("/notes/:id", (req, res) => {
     console.log("Delete Button HIT at ID:" + deleteID);
 
     for (let i = 0; i < notes.length; i++) {
-        
         if (notes[i].id = deleteID) {
             notes.splice(i, 1);
             console.log("Successful deletion");
             updateDatabase(notes);
-        } ;
+            res.sendFile(path.join(__dirname, '../../public/notes.html'))
+                     
+        };
     };   
 });
 
